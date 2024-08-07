f6d069f3de4cc842094e74c7274a5d8719843477
creinis <115181527+creinis@users.noreply.github.com>
2024-08-07 12:29:10 +0200

    feat: uncomment test cases

--- test_ports.py
+++ test_ports.py
@@ -5,13 +5,13 @@ def test_get_open_ports():
     result = port_scanner.get_open_ports("209.216.230.240", [440, 445])
     print("Open ports:", result)
 
-    """ print("\nTesting URL: www.stackoverflow.com")
+    print("\nTesting URL: www.stackoverflow.com")
     result = port_scanner.get_open_ports("www.stackoverflow.com", [79, 82])
-    print("Open ports:", result) """
+    print("Open ports:", result)
 
-    """ print("\nTesting URL with multiple ports: scanme.nmap.org")
+    print("\nTesting URL with multiple ports: scanme.nmap.org")
     result = port_scanner.get_open_ports("scanme.nmap.org", [20, 80])
-    print("Open ports:", result) """
+    print("Open ports:", result)
 
 if __name__ == "__main__":
     test_get_open_ports()
